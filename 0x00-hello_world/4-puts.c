b19cab9ae18d3db28ac6bcfa6c47c6c35abcbd53
CollinsNasongo <nasongocollins@gmail.com>
2022-06-30 22:04:58 +0300

    Print message using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main function - Entry point
+ * main - Entry point
  *
- * Return: Always 0 (Success) 
+ * Return: Always 0 (Succes)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 }
