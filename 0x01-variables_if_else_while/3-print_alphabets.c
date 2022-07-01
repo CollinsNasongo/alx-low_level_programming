c8fae6bd0c5c41394febb04e0e512e3fda21fa10
CollinsNasongo <nasongocollins@gmail.com>
2022-07-01 19:19:16 +0300

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	char alphabet;
 	char ALPHABET;
-
-        for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-        {
-                putchar(alphabet);
-        }
+	
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
 	for (ALPHABET = 'A'; ALPHABET <= 'Z'; ALPHABET++)
-        {
+	{
 		putchar(ALPHABET);
 	}
 	putchar('\n');
