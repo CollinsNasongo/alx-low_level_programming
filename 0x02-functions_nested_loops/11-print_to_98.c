d4a93d3d3c9a11eacb44be2ff028cf58c0ee469e
CollinsNasongo <nasongocollins@gmail.com>
2022-07-05 21:51:40 +0300

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,7 @@ void print_to_98(int n)
 			printf("%d, ", n);
 			n++;
 		}
-		_putchar('\n');
+		printf("%d\n", n);
 	}
 	else if (n > 98)
 	{
@@ -26,7 +26,7 @@ void print_to_98(int n)
 			printf("%d", n);
 			n--;
 		}
-		_putchar('\n');
+		printf("%d\n", n);
 	}
 	else
 		printf("%d", n);
