e7c70d36e072b8583724b46aff14969571c9905e
CollinsNasongo <nasongocollins@gmail.com>
2022-07-05 14:25:34 +0300

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print putchar followed by new line
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char words[] = "_putchar";
+	printf("%s\n", words);
+	return (0);
+}
