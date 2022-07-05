f1ce5b48af884e77856af84a8af5ced7d57de4b9
CollinsNasongo <nasongocollins@gmail.com>
2022-07-05 17:16:22 +0300

    Check if character is an alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,7 +8,7 @@
  * Return: 1 if the character is an alphabet
  * 0 if the character is not an alphabet
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
