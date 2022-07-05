a4a994322a169d687094c194d94bad6b331f600c
CollinsNasongo <nasongocollins@gmail.com>
2022-07-05 16:43:45 +0300

    Check for lowercase values

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,9 @@
 
 /**
  * _islower - checks if character is lowercase
+ *
+ * Return: returns 1 if the character is lowercase
+ * returns 0 if the character in not lowercase
  */
 int _islower(int c)
 {
