ac4bcd05bf2e31d4c51cea49bc4e0f39146354b6
CollinsNasongo <nasongocollins@gmail.com>
2022-07-08 22:21:11 +0300

    Print integer

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_triangle - use character # to print triangle
+ *
+ * @size: the size of the triangle
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	char space;
+}
