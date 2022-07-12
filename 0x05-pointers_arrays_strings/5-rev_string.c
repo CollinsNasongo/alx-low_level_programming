8b4f6067da72f4e86670bcbe6fb09a6f6b1f4358
CollinsNasongo <nasongocollins@gmail.com>
2022-07-12 03:50:32 +0300

    reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,19 +9,18 @@
  */
 void rev_string(char *s)
 {
-	int i, length;
+	int length = 0;
 	int index = 0;
-	char reverse[10000];
+	char rev;
 
-	while (s[index] != '\0')
+	while (s[index++])
 	{
-		index++;
+		length++;
 	}
-	length = index - 1;
-	for (i = 0; i < index; i++)
+	for (index = length - 1; index >= length / 2; index--)
 	{
-		reverse[i] = s[length];
-		length--;
+		rev = s[index];
+		s[index] = s[length - index - 1];
+		s[length - index - 1] = rev;
 	}
-	s = reverse;
 }
