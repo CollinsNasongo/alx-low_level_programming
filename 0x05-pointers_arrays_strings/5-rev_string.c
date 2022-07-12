6703c0c659ca24841f74ecdae5aa00794995e60a
CollinsNasongo <nasongocollins@gmail.com>
2022-07-12 03:38:30 +0300

    reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string to be reversed
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, length;
+	int index = 0;
+	char reverse[10000];
+
+	while (s[index] != '\0')
+	{
+		index++;
+	}
+	length = index - 1;
+	for (i = 0; i < index; i++)
+	{
+		reverse[i] = s[length];
+		length--;
+	}
+	*s = reverse;
+}
