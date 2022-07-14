5b8e1c06569497135de1d6e177b2a01a67cc144a
CollinsNasongo <nasongocollins@gmail.com>
2022-07-14 21:40:46 +0300

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,28 +9,31 @@
  */
 char *cap_string(char *str)
 {
-	int length, i;
+	int index = 0;
 
-	if (str == NULL)
-		return (NULL);
-
-	for (length = 0; str[length] != '\0'; length++)
-		;
-	if (str[0] != ' ' && str[0] != '\0')
-		if (str[0] >= 'a' && str[0] <= 'z')
-		{
-			str[0] = str[0] - 32;
-		}
-	for (i = 0; i < length; i++)
+	while (str[index])
 	{
-		if (str[i] == ' ')
-		{
-			if (i + 1 < length && str[i + 1] != ' ')
-				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-				{
-					str[i + 1] = str[i + 1] - 32;
-				}
-		}
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+
+		index++;
 	}
+
 	return (str);
 }
