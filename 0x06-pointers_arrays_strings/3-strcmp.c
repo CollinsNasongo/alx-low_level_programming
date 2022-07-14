7b97984fab501ba00dab3ceaf1626c043f7ffa4d
CollinsNasongo <nasongocollins@gmail.com>
2022-07-14 18:40:50 +0300

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,10 +15,10 @@ int _strcmp(char *s1, char *s2)
 	for (i = 0; (s1[i] != '\0' && s2[i] != '\0'); i++)
 	{
 		if (s1[i] < s2[i])
-			return (-15);
+			return (*s1 - *s2);
 		else if (s1[i] > s2[i])
-			return (15);
+			return (*s1 - *s2);
 		else
-			return (0);
+			return (*s1 - *s2);
 	}
 }
