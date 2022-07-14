87c253cede36a142bb6eb12c53da88652cd3028b
CollinsNasongo <nasongocollins@gmail.com>
2022-07-14 14:01:04 +0300

    Copy n characters

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,18 +11,11 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *pointer;
+	size_t i;
 
-	if (dest == NULL)
-		return (NULL);
-
-	pointer = dest;
-
-	while (*src && n--)
-	{
-		*dest++ = *src++;
-	}
-	*dest = '\0';
-
-	return (pointer);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
 }
