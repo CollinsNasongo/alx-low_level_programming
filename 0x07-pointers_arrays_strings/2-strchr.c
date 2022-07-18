2e81c10ce4a002fb4f122b22ce878b9ee435e09e
CollinsNasongo <nasongocollins@gmail.com>
2022-07-18 17:00:56 +0300

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,9 @@
  */
 char *_strchr(char *s, char c)
 {
-	int len = 0;
 	int i;
 
-	while (*s++)
-		len++;
-	for (i = 0; i <= len; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 			return (s + i);
