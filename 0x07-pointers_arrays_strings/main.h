d8f20f5c7985d526e170b8e41cc71a9c53d49809
CollinsNasongo <nasongocollins@gmail.com>
2022-07-18 08:08:28 +0300

    Task 0

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+/**
+ * This is a header file that contains a list of functions used
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /*MAIN_H*/
