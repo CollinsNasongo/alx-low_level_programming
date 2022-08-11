8d8c4ac119e2278f53b9c643055c8847bdbd56ce
CollinsNasongo <nasongocollins@gmail.com>
2022-08-12 01:09:04 +0300

    Task 2: add new node to the fromt

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+list_t *add_nde(list_t **head, const char *str);
+
+/**
+ * add_node - a function that adds new node to the beginning of list
+ *
+ * @head: the current head of the list
+ * @str: the string to be added to the beginning
+ *
+ * Return: pointer to the head
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	char *cpy = strdup(str);
+	unsigned int len = strlen(cpy);
+	list_t *new_node = malloc(sizeof(list_t));
+
+	new_node->str = cpy;
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
