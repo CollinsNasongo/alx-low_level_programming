148b8c9d3e50063a75880dc22e23950e46ece6ad
CollinsNasongo <nasongocollins@gmail.com>
2022-08-12 01:16:25 +0300

    Task 2: Add new node in the neginning (correction)

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,6 +16,14 @@ list_t *add_node(list_t **head, const char *str)
 	unsigned int len = strlen(cpy);
 	list_t *new_node = malloc(sizeof(list_t));
 
+	if (new_node == NULL)
+		return (NULL);
+	if (cpy == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
 	new_node->str = cpy;
 	new_node->len = len;
 	new_node->next = *head;
