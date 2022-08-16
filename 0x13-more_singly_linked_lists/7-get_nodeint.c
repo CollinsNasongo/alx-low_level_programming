9852d4bf164036b9795f1c7703f629356383072b
CollinsNasongo <nasongocollins@gmail.com>
2022-08-16 23:31:01 +0300

    Task 7: get nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,10 +12,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int pos;
 
-	if (head == NULL)
-		return (NULL);
 	for (pos = 0; pos != index; pos++)
 	{
+		if (head == NULL)
+			return (NULL):
 		head = head->next;
 	}
 	return (head);
