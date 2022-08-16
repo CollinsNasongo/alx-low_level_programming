56fc9eae855fff9548c5843f5116f3cd9a7213d8
CollinsNasongo <nasongocollins@gmail.com>
2022-08-17 02:13:23 +0300

    Task 9: Insert into list at a given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,10 +21,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	if (idx > (len - 1))
 		return (NULL);
-	for (pos = 0; pos != (idx - 1); pos++)
-		current = current->next;
 	new_node = malloc(sizeof(listint_t));
 	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = current;
+		*head = new_node;
+		return (new_node);
+	}
+	for (pos = 0; pos != (idx - 1); pos++)
+		current = current->next;
 	new_node->next = current->next;
 	current->next = new_node;
 	return (new_node);
--- 0x13-more_singly_linked_lists/test/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/test/9-insert_nodeint.c
@@ -13,18 +13,24 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int pos, len = 0;
 	listint_t *get_len = *head, *current = *head, *new_node;
-	
+
 	while (get_len != NULL)
 	{
 		len++;
 		get_len = get_len->next;
 	}
-	if (idx > (len-1))
+	if (idx > (len - 1))
 		return (NULL);
-	for (pos = 0; pos != (idx-1); pos++)
-		current = current->next;
 	new_node = malloc(sizeof(listint_t));
 	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = current;
+		*head = new_node;
+		return (new_node);
+	}
+	for (pos = 0; pos != (idx - 1); pos++)
+		current = current->next;
 	new_node->next = current->next;
 	current->next = new_node;
 	return (new_node);
