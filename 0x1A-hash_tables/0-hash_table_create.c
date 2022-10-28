046f1096862600d14ee3ba0c05bf4343cd87b313
CollinsNasongo <nasongocollins@gmail.com>
2022-10-28 10:58:20 +0300

    Create a new hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,13 +6,24 @@
  * @size: the size of the array
  *
  * Return: pointer to newly created hash table
+ * or NULL if failed
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = malloc(sizeof(hash_table_t) * size);
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	unsigned long int idx;
 	
 	if (new_table == NULL)
 		return (NULL);
+
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (new_table->array == NULL)
+		return (NULL);
+	for (idx = 0; idx < size; idx++)
+		new_table->array[idx] = NULL;
+
 	return (new_table);
 }
